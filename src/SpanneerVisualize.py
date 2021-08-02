d387a74eeb9d40c86bca0025c5ea712cbdbc4f23
amiralii007 <amiralikhanehanqa@gmail.com>
2021-08-02 16:01:36 +0430

    correcting names and comments and edit requirements

--- src/SpanneerVisualize.py
+++ src/SpanneerVisualize.py
@@ -9,11 +9,11 @@ from tkinter import *
 
 
 def graph_editor(grid=True, grid_N=24):
-    plt.ion()  # interactive mode -> figure1 hast ke mitonim bahash taamol konim
+    plt.ion()  # interactive mode 
     fig, ax = plt.subplots(1)
     ticks = np.linspace(0, 1, grid_N)
 
-    def init_ax(): # har seri miad kol safe ro pak mikone ke betonim chizaye jadid bezarim tosh
+    def init_ax(): # clean the picture for add new things
         ax.clear()
         ax.set_xlim(0, 1)
         ax.set_xticks(ticks)
@@ -24,17 +24,17 @@ def graph_editor(grid=True, grid_N=24):
         if grid:
             ax.grid()
 
-    # node ha ro vorodi migiram
+    # input nodes
     messagebox.showinfo("HI " , "Place the nodes and press enter")
     print("Place the nodes and press enter")
     init_ax()
-    fig.canvas.draw # shoro mikone be keshidan
-    nodes_pos = plt.ginput(-1, timeout=-1)  # get all input -> position ha ro migire, arg 1 -> tedad vorodi / arg 2 -> zaman ro limit mikone
+    fig.canvas.draw # creating canvas
+    nodes_pos = plt.ginput(-1, timeout=-1)  # get all input 
 
-    nodes_posx, nodes_posy = zip(*nodes_pos)    # x,y haye node ha ro to 2 ta list joda mirizam 
-    nodes = range(len(nodes_pos))   #tedad node ha baraye estefade dar algorithm
+    nodes_posx, nodes_posy = zip(*nodes_pos)  
+    nodes = range(len(nodes_pos))   #count nodes for algorithm
     
-    root = Tk() #ye window jadid baz mikone baraye vorodi gereftan k
+    root = Tk() #create new window to get inputs
     versiontitle = Label(root, text="input form")  
     versiontitle.pack() 
 
@@ -42,7 +42,7 @@ def graph_editor(grid=True, grid_N=24):
     k1text.pack()
     k1 = IntVar()  
     k1.set(5)
-    k1entry = Entry(root )  #entry yani textfield mizanim ro hamon window root va inke vorodi textfild ro mirize to in
+    k1entry = Entry(root )  #create textfield 
     k1entry.pack()
 
     j = 5
@@ -51,8 +51,8 @@ def graph_editor(grid=True, grid_N=24):
     def inbalg():
         j = k1entry.get()
         print(k1entry.get())
-        root.destroy()  #window ro mibande
-        root.quit() #az main loop darmiad
+        root.destroy()  #close window
+        root.quit() #finish loop
         
     magicbutton = Button(root , text='OK!', command=inbalg)
     magicbutton.pack()
@@ -62,29 +62,34 @@ def graph_editor(grid=True, grid_N=24):
 
     edges = []
 
-    for i in nodes:# ye node asli
-        for ang_obj in range(0, j): # har gheta
+    for i in nodes:#main node
+        for ang_obj in range(0, j): # angles
             reference_angle = 360 / j 
-            s_angle = ang_obj * reference_angle#zavie shoro har gheta ro darmiaram az 0 ham shoro mishe , 0 , 60 , 120 ,...(j=6)
-            e_angle = s_angle + reference_angle #payan gheta ro mide 60 , 120 , 180 (j=6)
+            # start each angle , 0 , 60 , 120 ,...(j=6)
+            s_angle = ang_obj * reference_angle
+            # end of each angle : 60 , 120 , 180 (j=6)
+            e_angle = s_angle + reference_angle 
             distance = sys.maxsize
-            for k in nodes: #tak tak node ha ro check mikonam nesbat be asli
+             #chack all nodes and compare with main node
+            for k in nodes:
                 dx, dy = nodes_posx[i] - nodes_posx[k], nodes_posy[i]-nodes_posy[k]
                 angle_in_radians = math.atan2(dx, dy)#ye zavie mide bar asas 2 noghte
                 if angle_in_radians<= 0:
-                    angle_in_radians = angle_in_radians + 2*pi  #ye moghe zavie manfi nade miaim + mikonamesh
+                    #if our angle is a negative number we spin that
+                    angle_in_radians = angle_in_radians + 2*pi  
                 angle_in_degrees = angle_in_radians * 180 / pi
                 if angle_in_degrees >= s_angle and angle_in_degrees <= e_angle and math.hypot(nodes_posx[i]-nodes_posx[k], nodes_posy[i]-nodes_posy[k]) > 0 and math.hypot(nodes_posx[i]-nodes_posx[k], nodes_posy[i]-nodes_posy[k]) < distance:
                     if(distance!=sys.maxsize):
-                        del edges[-1]#har seri ghabli ro pak mikonam age ghablan vorodi gereftam, akharin chizo mire pak mikone
+                        #each time delete the last one
+                        del edges[-1]
                     distance = math.hypot(nodes_posx[i]-nodes_posx[k], nodes_posy[i]-nodes_posy[k])
                     edges.append((i,k))
 
     #print(edges)
     fig.canvas.draw()
 
-
-    init_ax()#harchi ta alan hast ro refresh kon
+#Refresh all things in this step
+    init_ax()
     for i, j in edges:
         x1, y1 = nodes_pos[i]
         x2, y2 = nodes_pos[j]
@@ -107,10 +112,10 @@ if __name__ == '__main__':
 
     nodes, edges, nodes_pos = graph_editor()
 
-    G = nx.Graph()  # create graphe mamoli
+    G = nx.Graph()  # create normal graph
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
 
     fig, ax = plt.subplots(1)
     nx.draw(G, pos=dict(zip(nodes, nodes_pos)), ax=ax)
-    plt.show() #figure 2 ro mide
+    plt.show() #show second state
